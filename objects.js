185d2bc25779f381872cc7c2c1fbcff3d61e45f3
Zach Siegel <zachsiegel41@gmail.com>
2017-09-15 21:39:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,8 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = "Phil Ochs";
-  playlist.songTitle = "Here's to the State of Mississippi"
+  playlist[Phil Ochs] = "Here's to the State of Mississippi";
   return Object.assign({}, playlist, updatePlaylist)
 }
 
