25f70eb98ec8447c6038fd46cc81137440ad5313
Zach Siegel <zachsiegel41@gmail.com>
2017-09-15 21:41:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, updatePlaylist)
 }
 
-function removeFromPlaylist(playlist, removeFromPlaylist) {
+function removeFromPlaylist(playlist, artistName, songTitle) {
   playlist.artistName = "Kanye";
   playlist.songTitle = "Gold Digger";
   return Object.assign({}, playlist, removeFromPlaylist)
