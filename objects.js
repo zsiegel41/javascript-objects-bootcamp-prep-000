d0cf078aa551781e04600ab1d384cc8f89c10115
Zach Siegel <zachsiegel41@gmail.com>
2017-09-15 21:33:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   
 
-  return Object.assign({Object(Phil Ochs)}, playlist, updatePlaylist)
+  return Object.assign({}, playlist, updatePlaylist)
 }
 
 function removeFromPlaylist(playlist, removeFromPlaylist) {
